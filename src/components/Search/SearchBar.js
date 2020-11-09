178acc76eedb1ae65d601fc8bab836ce8223efe1
Grant Pardington <grant.pardington@gmail.com>
2020-11-08 18:09:27 -0700

    Reorganizing files. Fixing search bar

---
+++ src/components/Search/SearchBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./style.css";
+
+function SearchBar({ handleInputChange }) {
+  return (
+    <div className="container mt-5">
+      <div className="row col-12 justify-content-center">
+        <form className="form-inline input-group p-2 searchbar">
+          <input
+            className="form-control"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            onChange={handleInputChange}
+          />
+          <button
+            className="btn btn-outline-success ml-2"
+            type="submit"
+            onClick={handleInputChange}>
+            <i className="fas fa-search"></i>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
