f5a44b6694d18a26f4a1eb407b0d97ea46231104
Grant Pardington <grant.pardington@gmail.com>
2020-11-08 18:11:03 -0700

    Fixing search bar

---
+++ src/components/Search/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+
+function Search({ handleInputChange }) {
+  return <SearchBar handleInputChange={handleInputChange} />;
+}
+
+export default Search;
